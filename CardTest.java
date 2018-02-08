820e0d4bb09c1329354410dadf73a6de767a93ac
Hancock <bchancock.2017@mymail.jcsu.edu>
2018-02-08 14:12:22 -0500

    first

--- CardTest.java
+++ CardTest.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner ;
 public class CardTest {
 
 	public static void main(String[] args) {
@@ -15,6 +15,29 @@ public class CardTest {
 		System.out.println(card2Type1.Number + card2Type1.Suit);
 		System.out.println(Lucky.Number + Lucky.Suit);
 		System.out.println(card2Type3.Number + card2Type3.Suit);
+		System.out.println(cardType1.Number + cardType1.Suit);
+		System.out.println(card3Type1.Number + card3Type1.Suit);
+		System.out.println(cardType2.Number + cardType2.Suit);
+		System.out.println(card2Type2.Number + card2Type2.Suit);
+		System.out.println(card2Type3.Number + card2Type3.Suit);
+		System.out.println(card3Type3.Number + card3Type3.Suit );
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println ("What suit do you want");
+		String Card2 = scanner.nextLine();
+		
+		System.out.println("Which number?");
+		int Card =scanner.nextInt();
+		
+		if (Card == Lucky.Number && Card2.equals(Lucky.Suit)){
+			System.out.println ("You win");
+		}
+		else {
+			System.out.println("You Lose");
+		}
+		
+		
 	}
+	
 
 }
